8000a901d44169ea15a1001d1580089112d5ac7d
BoardCogs <gianmaria.forte2002@gmail.com>
2022-05-03 23:09:49 +0200

    Modifica gimmy-andre (write)

--- sistemi_operativi/system_call/fifo.h
+++ sistemi_operativi/system_call/fifo.h
@@ -6,12 +6,16 @@
 
 // The create_fifo create a fifo or
 // terminate process if there is an error
-void create_fifo (const char *pathname, mode_t permissions); // !!! Da provare se funziona permissions = S_IRUSR | S_IWUSR (esempio) !!!
+void create_fifo (const char *pathname); // !!! Da provare se funziona permissions = S_IRUSR | S_IWUSR (esempio) !!!
 
 // Open a fifo and return a file descriptor,
 // terminate process if there is an error
 int open_fifo(const char *pathname, int flags); // !!! Si può ottimizzare dato che alla fin fine la open non la usa solo la FIFO, quindi si potrebbe definire in define.h !!!
 
-// Read a fifo or
+// Read from fifo or
 // terminate process if there is an error
-void read_fifo(int fd, char *buf, size_t bytes_to_read);
+void read_fifo (int fd, void *buf, size_t bytes_to_read);
+
+// Write on fifo or
+// terminate process if there is an error
+void write_fifo (int fd, void *buf, size_t bytes_to_read);
