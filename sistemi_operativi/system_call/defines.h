3080caf799a919231b75dd426850400c9de42a22
AndreVale69 <57899285+AndreVale69@users.noreply.github.com>
2022-05-09 09:23:23 -0700

    Add files via upload

---
+++ sistemi_operativi/system_call/defines.h
@@ -0,0 +1,27 @@
+/// @file defines.h
+/// @brief Contiene la definizioni di variabili
+///         e funzioni specifiche del progetto.
+
+#pragma once
+
+#include <sys/types.h>
+#include <linux/limits.h>
+
+// the my_msg structure defines a file fregment sent by a client
+struct queue_msg {
+    long mtype;
+    pid_t pid;
+    char pathname[PATH_MAX];
+    char fragment[1025];            // one more char to compensate for string terminator
+    // DA PROVARE TERMINATORE
+};
+
+void check_malloc (void *pointer);
+
+/*
+struct general_msg {
+    pid_t pid;
+    char pathname[250];     // assuming pathnames shorter than 250 Bytes
+    char fragment[1001];    // one more char to compensate for string terminator
+}
+*/
